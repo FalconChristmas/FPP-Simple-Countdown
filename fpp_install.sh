11c9ee83bbd9f03b4bd48924dd493d102b276839
jessica12ryan <jessica12ryan@gmail.com>
2026-08-10 02:05:40 -0300

    Fix lifecycle robustness, log naming, and HTTP timeouts

--- fpp_install.sh
+++ fpp_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
 pushd $(dirname $(which $0))
 target_PWD=$(readlink -f .)
 echo ; echo “Please restart fppd for new FPP Commands to be visible.” ; echo
 . /opt/fpp/scripts/common
+# Remove log file left behind by versions before the plugin-*.log convention
+[ -d "${LOGDIR}" ] && rm -f "${LOGDIR}/FPP-Simple-Countdown.log" || true
 setSetting restartFlag 1
 popd
